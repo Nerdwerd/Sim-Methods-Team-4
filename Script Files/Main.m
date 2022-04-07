b8cadc6005a68188f12987518f6f3bbfe98d4db1
Nerdwerd <jackson.harros@yahoo.com>
2022-04-07 14:45:29 -0500

    Set up Basic Function

--- Script Files/Main.m
+++ Script Files/Main.m
@@ -3,7 +3,7 @@
 
 %https://www.rapidtables.com/web/color/purple-color.html
 
-I = imread("Images\Image3.jpg");
+I = imread("Images\Image1.jfif");
 
 test = [I(10,10,1),I(10,10,2),I(10,10,3)]
 
@@ -11,3 +11,22 @@ x = size(I)
 
 imshow(I)
 
+
+
+
+function ImageOut = P2Y(ImageIn)
+    
+    cvalues = size(ImageIn); %Size of the image by row, column, and RGB values
+
+    pvalues = [0;0;0]; %Purple Range to compare
+    yvalues = [0;0;0]; %Yellow color to set
+
+    %Runs through each part of the Image matrix
+    for r = 0:cvalues(1)
+        for c = 0:cvalues(2)
+            for rbg = 0:cvalues(3)
+
+            end
+        end
+    end
+end
