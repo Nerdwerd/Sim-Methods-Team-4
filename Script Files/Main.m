ba7b91e94259e552a3e53952fa9afd2e1462ddf8
Nerdwerd <jackson.harros@yahoo.com>
2022-04-08 15:54:20 -0500

    23

--- Script Files/Main.m
+++ Script Files/Main.m
@@ -3,12 +3,12 @@
 
 %https://www.rapidtables.com/web/color/purple-color.html
 
-I = imread("Images\Image1.jfif");
+I = imread("Images\Image4.png");
 
-test = [I(10,10,1),I(10,10,2),I(10,10,3)]
 
 x = size(I)
 O = P2Y(I);
+
 imshow(O)
 
 
@@ -24,7 +24,7 @@ function ImageOut = P2Y(ImageIn)
     for r = 1:cvalues(1)
         for c = 1:cvalues(2)
             
-            if (ImageIn(r,c,1) < 140) && (ImageIn(r,c,2) < 85) && (ImageIn(r,c,3) < 200)
+            if (100 < ImageIn(r,c,1) && ImageIn(r,c,1) < 195) && (50 < ImageIn(r,c,2) && ImageIn(r,c,2) < 90) && (190 < ImageIn(r,c,3) && ImageIn(r,c,3) < 240)
                 ImageIn(r,c,1) = yvalues(1);
                 ImageIn(r,c,2) = yvalues(2);
                 ImageIn(r,c,3) = yvalues(3);
