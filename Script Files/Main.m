e3a36a41e5a4679ff1756b247ab9927a3b89555b
Nerdwerd <jackson.harros@yahoo.com>
2022-04-20 17:59:51 -0500

    Done

--- Script Files/Main.m
+++ Script Files/Main.m
@@ -9,17 +9,15 @@ I2 = imread("Images\Image2.png");
 I3 = imread("Images\Image3.jpg");
 I4 = imread("Images\Image4.png");
 
-x = size(I1)
-%O = P2Y(I);
-Y = P2Y(I1);
-%imshow(O);
+Y = P2Y(I4);
+
 imshow(Y);
 
 
 
 
 function ImageOut = P2Y(ImageIn)
-    
+    f = 0;
     cvalues = size(ImageIn); %Size of the image by row, column, and RGB values
     
     yvalues = [255;255;0]; %Yellow color to set
@@ -31,7 +29,8 @@ function ImageOut = P2Y(ImageIn)
         for c = 1:cvalues(2)
             rg = ImageIn(r,c,1)/ImageIn(r,c,2);
             bg = ImageIn(r,c,3)/ImageIn(r,c,2);
-            if ((rg >= 1.1) && (bg >= 1) && ImageIn(r,c,1) > 90 && ImageIn(r,c,3) > 90)
+            br = ImageIn(r,c,3)/ImageIn(r,c,1);
+            if ((rg >= 1.1) && (bg >= 1.1) && (ImageIn(r,c,1) > 30 && ImageIn(r,c,3) > 30))
                 ImageIn(r,c,1) = yvalues(1);
                 ImageIn(r,c,2) = yvalues(2);
                 ImageIn(r,c,3) = yvalues(3);
@@ -42,13 +41,17 @@ function ImageOut = P2Y(ImageIn)
 
     for r = 1:cvalues(1)
         for c = 1:cvalues(2)
-            
-            if (ImageIn(r,c,1) > 10) && (ImageIn(r,c,2) < 60) && (ImageIn(r,c,3) > 100)
+            t1 = abs(ImageIn(r,c,1) - ImageIn(r,c,2));
+            t2 = abs(ImageIn(r,c,1) - ImageIn(r,c,3));
+            if t1 < 3 && t2 < 3
+                f = 1;
+            end
+            if (ImageIn(r,c,1) > 90) && (ImageIn(r,c,3) > 100) && (ImageIn(r,c,2) < 230) && f ~= 1 
                 ImageIn(r,c,1) = yvalues(1);
                 ImageIn(r,c,2) = yvalues(2);
                 ImageIn(r,c,3) = yvalues(3);
             end
-            
+            f = 0;
         end
     end
 
